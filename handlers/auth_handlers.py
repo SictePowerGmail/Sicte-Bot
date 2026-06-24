80c2691ba1322dc379a4577002d3214a21810db5
SictePowerGmail <brayanbarrera.sicte@gmail.com>
2026-06-24 12:32:28 -0500

    Mensaje de bienvenida mejorado

--- handlers/auth_handlers.py
+++ handlers/auth_handlers.py
@@ -100,7 +100,7 @@ def register_auth_handlers(bot: TeleBot):
         user = auth_service_instance.autenticar_usuario(cedula, password, message.from_user.id)
         if user:
             roles_str = ", ".join(user.roles) if user.roles else "Sin roles"
-            bot.send_message(message.chat.id, f"¡Sesión iniciada exitosamente!\nBienvenido {user.primer_nombre} ({roles_str})")
+            bot.send_message(message.chat.id, f"¡Sesión iniciada exitosamente!\nBienvenido {user.primer_nombre} \nTienes acceso a: ({roles_str})")
             bot.delete_state(message.from_user.id, message.chat.id)
             if message.from_user.id in login_temp_data:
                 del login_temp_data[message.from_user.id]
